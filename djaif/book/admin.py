c92e64e6a6d9621ea1dec8a7dea9e07d85182cac
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-05-26 18:31:08 +0300

    tweak admin site slightly

--- djaif/book/admin.py
+++ djaif/book/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 from djaif.book import models
 
+class Admin(admin.ModelAdmin):
+    filter_horizontal = ('items',)
+
 admin.site.register(models.Book)
-admin.site.register(models.BookPage)
-admin.site.register(models.PageLink)
+admin.site.register(models.BookPage, Admin)
+admin.site.register(models.PageLink, Admin)
 admin.site.register(models.BookProgress)
 admin.site.register(models.Item)
--- djaif/book/models.py
+++ djaif/book/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.TextField(name='title', unique=True)
+    title = models.CharField(name='title', max_length=100, unique=True)
     first_page = models.ForeignKey(
         'BookPage', null=True, on_delete=models.SET_NULL,
         related_name='first_name',
     )
-    cover_art = models.ImageField(null=True)
+    cover_art = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return '{self.title} ({self.id})'.format(self=self)
@@ -17,7 +17,7 @@ class Book(models.Model):
 
 class BookPage(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    title = models.TextField(name='title')
+    title = models.CharField(name='title', max_length=100)
     body = models.TextField(name='body')
 
     items = models.ManyToManyField('book.Item', blank=True)  # noqa: WPS110
