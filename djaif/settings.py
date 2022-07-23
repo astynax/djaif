7bd2ab6e5e76ce3193ac86f104c4ec35914de4f7
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-07-23 11:37:07 +0300

    fix(settings): remove default value with debug key

--- djaif/settings.py
+++ djaif/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DJANGO_SETTINGS_MODULE = 'djaif.settings'
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = bool(os.getenv('DEBUG'))
 SUPERUSER = os.getenv('SUPERUSER', 'admin')
 PASSWORD = os.getenv('PASSWORD', 'admin')
 
